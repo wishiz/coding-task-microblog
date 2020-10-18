a03c2fb2bc365698117c26f103e658fc5a8a51e0
wishiz <eugeniavoloshina@gmail.com>
2020-10-18 19:57:02 +0300

    Updates

---
+++ precache-manifest.8fb1d2a5fe4fb6f9facaf779c9ebf820.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcc39220607b67cd869e9af8bf05f02",
+    "url": "/coding-task/index.html"
+  },
+  {
+    "revision": "7477f057a0a00aef147f",
+    "url": "/coding-task/static/css/main.07596c28.chunk.css"
+  },
+  {
+    "revision": "f2ca0ab33b664bca1191",
+    "url": "/coding-task/static/js/2.9789b608.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/coding-task/static/js/2.9789b608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7477f057a0a00aef147f",
+    "url": "/coding-task/static/js/main.ebd8412e.chunk.js"
+  },
+  {
+    "revision": "5a04bb024036d3a2153d",
+    "url": "/coding-task/static/js/runtime-main.e32ee164.js"
+  },
+  {
+    "revision": "1534a77dfb56f7a6245a31c07785ba72",
+    "url": "/coding-task/static/media/user10_pic.1534a77d.jpg"
+  },
+  {
+    "revision": "0fab41e76dde4ed6c17bfbf2d456bc8d",
+    "url": "/coding-task/static/media/user1_pic.0fab41e7.jpg"
+  },
+  {
+    "revision": "1734cfc696d83aab3bc3692571061ce5",
+    "url": "/coding-task/static/media/user4_pic.1734cfc6.jpg"
+  },
+  {
+    "revision": "5409212355d4162d226aa8c1645b0b00",
+    "url": "/coding-task/static/media/user6_pic.54092123.jpg"
+  },
+  {
+    "revision": "01ca940d4b90707f9cecb531669f655d",
+    "url": "/coding-task/static/media/user7_pic.01ca940d.jpg"
+  }
+]);
